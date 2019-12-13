3f228fffe397e4b9e8d7baf4ccc6bb7e5c1a3d28
michael <lukyanovmiv@gmail.com>
2019-12-13 13:41:51 +0300

    new commit

--- src/main/java/ru/lukymiv/doc/createHTML.java
+++ src/main/java/ru/lukymiv/doc/createHTML.java
@@ -1,6 +1,7 @@
 package ru.lukymiv.doc;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.apache.commons.io.FileUtils;
@@ -13,16 +14,32 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
 
 public class createHTML {
 
-    static String[] urls = new String[]{"General_info",
+    static String[] inter = new String[]{
+            "Main_page",
+            "Resource_monitor",
+            "Send_message",
+            "Queue_manager",
+            "Database",
+            "Logs",
+            "Settings",
+            "User_page",
+            "Dashboard",
+            "Web_services",
+            "REST"};
+
+    static String[] urls = new String[]{
+            "General_info",
             "Purpose_and_scope",
-            "Architecture",
-            "Architecture",
             "Architecture",
             "Main_page",
             "Resource_monitor",
@@ -35,17 +52,20 @@ public class createHTML {
             "Dashboard",
             "Web_services",
             "REST",
-            "Transaction_monitor",
-            "REST_API",
             "Install",
             "Modes_exp",
             "Attachment"};
+
 
     public static void main(String[] args) throws IOException {
         downloadFiles();
         File input = new File("/home/michael/IdeaProjects/adminMan/src/main/recources/files/Queue_manager.html");
         Document doc = Jsoup.parse(input, "UTF-8", "");
         Element head = doc.head();
+        head.select("style").get(0).append("      table, th, td {\n" +
+                "          border: 1px solid black;\n" +
+                "          border-collapse: collapse;\n" +
+                "      }");
         Document finalDoc = Document.createShell("");
         finalDoc.select("head").append(head.html());
         finalDoc.select("body").append("<div id=\"app\"><div class=\"body-container\"><div class=\"documentation\"></div></div></div>");
@@ -53,12 +73,19 @@ public class createHTML {
             File data = new File("/home/michael/IdeaProjects/adminMan/src/main/recources/files/"+name+".html");
             Document docData = Jsoup.parse(data, "UTF-8", "");
             String content = docData.select("div.documentation").html();
+            if (Arrays.asList(inter).contains(name)) {
+                content.replaceAll("1", "2");
+            }
             finalDoc.select("div.documentation").append(content);
         }
         final File f = new File("filename.html");
-        FileUtils.writeStringToFile(f, finalDoc.outerHtml(), "UTF-8");
+        FileUtils.writeStringToFile(f, finalDoc.outerHtml().replaceAll("img src=\"/images", "img src=\"./images"), "UTF-8");
         System.out.println();
 
+    }
+
+    public static void replace (int whatReplace, String content) {
+        whatReplace += 1;
     }
 
     private static void downloadFiles() throws FileNotFoundException {
@@ -70,6 +97,14 @@ public class createHTML {
         LoginPage.loginClickButton("root", "root");
         $x("//a[@href=\"#/doc\"]").click();
         WebDriver driver = WebDriverRunner.getWebDriver();
+        Set<String> winSet = WebDriverRunner.getWebDriver().getWindowHandles();
+        List<String> winList = new ArrayList<String>(winSet);
+        String currentWindow = WebDriverRunner.getWebDriver().getWindowHandle();
+        for (String win : winList) {
+            if (!win.equals(currentWindow)) {
+                Selenide.switchTo().window(win);
+            }
+        }
         for (int i=0; i<urls.length; i++) {
             String fileName = urls[i];
             $x("//a[@href=\"#/doc/"+fileName+"\"]").click();
